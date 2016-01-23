f72473a9253c3ff305dd88f11e121d23d5c6e389
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2016-01-23 11:25:39 -0500

    added end points

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -2,6 +2,14 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
 var app        = express();
+var Parse = require('node-parse-api').Parse;
+
+var options = {
+	app_id : "",
+	api_key : "",
+};
+
+var parse = new Parse(options);
 
 var groceries = {
 	"beef" : [1,2],
@@ -45,9 +53,10 @@ router.use(function(req, res, next) {
 });
 
 router.get('/', function(req, res){
-	res.json({message: "working"});
+	res.sendfile(res.sendfile('../site/index.html', {root: __dirname }));
 });
 
+// needs an abreviation to look up (:abrv)
 router.route('/abrv/:abrv')
 	.get(function(req, res){
 		var result = (match(req.params.abrv));
@@ -59,7 +68,48 @@ router.route('/abrv/:abrv')
 
 	});
 
+// needs a data field in the body with the list from the OCR algorithm 
+router.route('/add/multiple')
+	.post(function(req, res) {
+		var data = req.body.data;
+		var prods = determineProducts(data);
+		for (var prod in prods) {
+			var expirationd = getNDaysFromNow(groceries[prod][0]);
+			var lifetime = groceries[prod][0];
+			var jsonObj = {
+				"name" : name,
+				"expiration-date" : expirationd,
+				"lifetime" : lifetime
+			}
 
+			addItemToParse(jsonObj);
+		}
+
+		res.json("done");
+	});
+
+router.route('add/single')
+	.post(function(req, res) {
+		var name = match(req.body.name);
+		if (name == null) {
+			res.json("Provide a valid name");
+			res.send();
+		} else {
+			var expirationd = getNDaysFromNow(groceries[name][0]);
+			var lifetime = groceries[name][0];
+			var jsonObj = {
+				"name" : name,
+				"expiration-date" : expirationd,
+				"lifetime" : lifetime
+			}
+
+			addItemToParse(jsonObj);
+		}
+
+		res.json("done");
+	});
+
+// post needs fields for each col in parse (name, expiration date, lifetime)
 app.use('/api', router);
 
 
@@ -77,7 +127,7 @@ function match(text){
 		resultingClosestWord = [abrv[a[0]], a[1]];
 	}
 
-	if (resultingClosestWord[1] > Math.min(text.length - 1, resultingClosestWord[0].length - 1)) {
+	if (resultingClosestWord[1] > Math.min(text.length / 2 , resultingClosestWord[0].length / 2 )) {
 		return null;
 	} else {
 
@@ -107,14 +157,14 @@ function determineProducts(list) {
 		var item = list[i];
 		var res = match(item);
 		if (res != null) {
-			console.log(item + "is also known as: " + res);
+			console.log(item + " is also known as: " + res);
 			if (res in dict) {
 				dict[res]++;
 			} else {
 				dict[res] = 1;
 			}
 		} else {
-			console.log(item + " has no match.");
+			//console.log(item + " has no match.");
 		}
 	}
 
@@ -125,7 +175,7 @@ function getClosestWord(w, dict) {
 	var minDis = -1;
 	var minWord = "";                                                
 	for (var grocery in dict) {
-		var dis = getEditDistance(w, grocery);
+		var dis = getEditDistance(w.toLowerCase(), grocery.toLowerCase());
 		//console.log(grocery + " the dist is " + dis);
 		if (minDis == -1 || minDis > dis) {
 			minDis = dis;
@@ -213,7 +263,26 @@ function removeAllVowels() {
 	console.log(arr);
 }
 
-var test = ["‘I‘INEIIIINIIHIIIIIL", "1", "\h", "1", "Save", "money.", "Live", "better.", 
+function addItemToParse(food) {
+	var item = {
+		"name" : food,
+		"expiration-date" : getNDaysFromNow(groceries[food][0]),
+		"lifetime" : groceries[food],
+		"description" : ""
+	}
+
+	app.insert('FoodEntry', item, function (err, response) {
+	  console.log(response);
+	});
+}
+
+function getNDaysFromNow(n) {
+	var a = new Date();
+	a.setDate(a.getDate() + n);
+	return a;
+}
+
+/*var test = ["‘I‘INEIIIINIIHIIIIIL", "1", "\h", "1", "Save", "money.", "Live", "better.", 
 			"I", "085R)S$EOIR", "30588", "Ran", "13059", "FHIR", "LHKES", "PnRKuRY", "FHIRFRX", 
 			"vn", "22033", "a", "518", "2016", "0=a", "00004960", "16:", "11", "1R1", "0190", "8/8", "BRST", 
 			"L3", "020646690943", "F", "43", "1", "8/5", "BRST", "L3", "020546611017", "F", "T", "g", "SUBTOTAL", 
@@ -221,8 +290,10 @@ var test = ["‘I‘INEIIIINIIHIIIIIL", "1", "\h", "1", "Save", "money.", "Live"
 			"F", "49", "6", "n/s", "BRST", "L3", "020646681031", "F", "1", "3", ";", "a/s", "BRST", "Ls", 
 			"020546681075", "F", "1", ".7", ".", "SUBTOTRL", "6", "6", ";", "nc", "CELERY", "SE", "006210000218",
 			 "F", ".", "no", "CELERY", "SE", "006210000218", "F", "HCC/SCH", "PICK", "006210000602", "F", 
-			 "HCC/SCH", "PICK", "006210000602", "F", "CHICKN", "BROTH", "006100013279"];
-console.log(determineProducts(test));
+			 "HCC/SCH", "PICK", "006210000602", "F", "CHICKN", "BROTH", "006100013279"];*/
+
+/*var test = ["W§", "BRUCERV", "§", "2!;", "Mixed", "Nuts", "19,99", "1550", "x", "125,00/kg", "yitKat", "A-Fingev", "45g“", "7,00", "Trnp.", "Apple", "Juice", "7501", "11,00", "0119‘", "Cam", "Flakes", "7500", "8%", "25,50", "m1.\§v|15n", "§m", "chicken", "%", "42,00", "262g", "W", "Banana", "Chiqulta", "9,20", "452g", "x", "18,50/kg", "urange", "7,35", "2850", "x", "21,00/kg", "Mushroum", "6,85", "170g", "x", "as", "nu/kg"];
+console.log(determineProducts(test));*/
 
 // START THE SERVER
 // =============================================================================
