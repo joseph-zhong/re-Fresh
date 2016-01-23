41cb1f94136eaaeaa4f1f2f278d5afeee99485fb
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2016-01-23 05:12:56 -0500

    package.json and server updated

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -14,8 +14,18 @@ var groceries = {
 	"potatoes" : [21, 35],
 	"broccoli" : [7, 14],
 	"bread" : [5, 7]
-}
+};
 
+var abrv = {'bf' : 'beef',
+  'mlk' : 'milk',
+  'chckn': 'chicken',
+  'bcn': 'bacon',
+  'slmn': 'salmon',
+  'ppls' : 'apples',
+  'rngs' : 'oranges',
+  'ptts' : 'potatoes',
+  'brccl' : 'broccoli',
+  'brd' : 'bread'};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -37,9 +47,15 @@ router.get('/', function(req, res){
 	res.json({message: "working"});
 });
 
-router.route('/route/abrv')
+router.route('/abrv/:abrv')
 	.get(function(req, res){
-		match(req.params.abrv);
+		var result = (match(req.params.abrv));
+		if (result == null) {
+			res.json("No Match found");
+		} else {
+			res.json(result + " and the expiration date is in " + groceries[result][0] + " day(s)");
+		}
+
 	});
 
 
@@ -50,7 +66,25 @@ function match(text){
 	var len=text.length;
 	var minLength=100;
 	var result="";
-	for(var grocery : groceries){
+	var w = getClosestWord(text, groceries);
+	var a = getClosestWord(text, abrv);
+	var resultingClosestWord;
+	
+	if (w[1] < a[1]) {
+		resultingClosestWord = w;
+	} else {
+		resultingClosestWord = [abrv[a[0]], a[1]];
+	}
+
+	if (resultingClosestWord[1] >= text.length - 1) {
+		return null;
+	}
+
+	console.log(resultingClosestWord);
+
+	return resultingClosestWord[0];
+	
+	/*for(var grocery in groceries){
 		var pointer=0;
 		for(i=0;i<grocery.length;i++){
 			if(text.charAt(pointer)==grocery.charAt(i)){
@@ -61,9 +95,103 @@ function match(text){
 			minLength=grocery.length-len;
 			result=grocery;
 		}
-	}
-	return result;
+	} */
+
+//	return result;
 }
+
+function getClosestWord(w, dict) {
+	var minDis = -1;
+	var minWord = "";
+	for (var grocery in dict) {
+		var dis = getEditDistance(w, grocery);
+		//console.log(grocery + " the dist is " + dis);
+		if (minDis == -1 || minDis > dis) {
+			minDis = dis;
+			minWord = grocery;
+		}
+	}
+
+	return [minWord, minDis];
+}
+
+/*function getClosestAbrev(w) {
+	var minDis = -1;
+	var minWord = "";
+	for (var grocery in abrv) {
+		grocery = abrv[grocery];
+		var dis = getEditDistance(w, grocery);
+		//console.log(grocery + " the dist is " + dis);
+		if (minDis == -1 || minDis > dis) {
+			minDis = dis;
+			minWord = grocery;
+		}
+	}
+
+	return [minWord, minDis];
+} */
+
+function levenshteinDistance (s, t) {
+    if (!s.length) return t.length;
+    if (!t.length) return s.length;
+
+    return Math.min(
+        levenshteinDistance(s.substr(1), t) + 1,
+        levenshteinDistance(t.substr(1), s) + 1,
+        levenshteinDistance(s.substr(1), t.substr(1)) + (s[0] !== t[0] ? 1 : 0)
+    ) + 1;
+}
+
+function getEditDistance(a, b){
+  if(a.length == 0) return b.length; 
+  if(b.length == 0) return a.length; 
+
+  var matrix = [];
+
+  // increment along the first column of each row
+  var i;
+  for(i = 0; i <= b.length; i++){
+    matrix[i] = [i];
+  }
+
+  // increment each column in the first row
+  var j;
+  for(j = 0; j <= a.length; j++){
+    matrix[0][j] = j;
+  }
+
+  // Fill in the rest of the matrix
+  for(i = 1; i <= b.length; i++){
+    for(j = 1; j <= a.length; j++){
+      if(b.charAt(i-1) == a.charAt(j-1)){
+        matrix[i][j] = matrix[i-1][j-1];
+      } else {
+        matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+                                Math.min(matrix[i][j-1] + 1, // insertion
+                                         matrix[i-1][j] + 1)); // deletion
+      }
+    }
+  }
+
+  return matrix[b.length][a.length];
+};
+
+function removeAllVowels() {
+	var arr = [];
+	for (var g in groceries) {
+		var res = "";
+		for (var i = 0; i < g.length; i++) {
+			//console.log(g[i]);
+			var c = g[i];
+			if (['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) == -1) {
+				res += g[i];
+			}
+		}
+		arr.push(res);
+	}
+	console.log(arr);
+}
+
 
 // START THE SERVER
 // =============================================================================
