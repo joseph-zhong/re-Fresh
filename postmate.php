16c9774e6fa67b8ac32974231a0153716bbeda73
Winston Wright <winstonwright88@gmail.com>
2016-01-24 14:26:45 +0000

    Final php

--- postmate.php
+++ postmate.php
@@ -1,15 +1,19 @@
 <?php
+header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Methods: GET, POST');
 $url = 'https://api.postmates.com/v1/customers/cus_KeAkAy7GIWj1lF/delivery_quotes';
 $key="51a61f0e-4833-4745-9f3d-c48853e76a71";
 $fields = array(
 	'pickup_address' => urlencode($_POST['pickup_address']),
+	'dropoff_address' => urlencode($_POST['dropoff_address'])
+	/*'pickup_address' => urlencode($_POST['pickup_address']),
 	'pickup_phone_number' => urlencode($_POST['pickup_phone_number']),
 	'pickup_notes' => urlencode($_POST['pickup_notes']),
 	'dropoff_name' => urlencode($_POST['dropoff_name']),
 	'dropoff_address' => urlencode($_POST['dropoff_address']),
 	'dropoff_phone_number' => urlencode($_POST['dropoff_phone_number']),
 	'dropoff_notes' => urlencode($_POST['dropoff_notes']),
-	'manifest' => urlencode($_POST['manifest']
+	'manifest' => urlencode($_POST['manifest'])*/
 );
 
 $headers = array(
